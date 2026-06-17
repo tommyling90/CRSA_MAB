982cd6f308edd193777fff13aa5ca419aa976532
Tommy <43625140+tommyling90@users.noreply.github.com>
2026-06-17 16:52:52 -0400

    réorganiser le code pour réduire des loops

--- src/crsa/speaker.py
+++ src/crsa/speaker.py
@@ -3,16 +3,15 @@ from src.priors.priors import y_prior_given_mS_mL, compatible
 from src.crsa.listener import get_lit_listener_dist
 from src.transforms.matrix_to_meanings import generate_meaning_space
 
-def get_speaker_score(m_S, tau_S, tau_L, u, Y_space, y_opt, k, n, w):
+def get_speaker_score(m_S, tau_S, tau_L, u, Y_space, y_opt, k, n, w, joint_belief_den, Ml_space):
     #dummy value to avoid log(0)
     dummy = 1e-12
-    Ml_space = list(generate_meaning_space(k, n))
     score = 0.0
 
     for cand_m_L in Ml_space:
         y_dist = get_lit_listener_dist(cand_m_L, tau_S, tau_L, u, Y_space, y_opt, k, n, w)
         for y in Y_space:
-            joint_prob = joint_belief(m_S, cand_m_L, tau_S, tau_L, y, y_opt, Ml_space, w)
+            joint_prob = joint_belief(m_S, cand_m_L, tau_S, tau_L, y, y_opt, joint_belief_den, w)
             prob_y = y_dist[y]
             #TODO: need to think up a cost function in the value below
             value = math.log(prob_y + dummy)
@@ -22,10 +21,22 @@ def get_speaker_score(m_S, tau_S, tau_L, u, Y_space, y_opt, k, n, w):
     return score
 
 def get_speaker_dist(m_S, tau_S, tau_L, U_space, Y_space, y_opt, k, n, alpha=1.0, w=None):
+    # calculer le dénominateur pour le belief conjoint ici pour éviter un loop non-nécessaire
+    Ml_space = list(generate_meaning_space(k, n))
+    denominator = sum(
+        belief(w)
+        * compatible(m_S, cand_m_L, tau_S, tau_L, y_opt)
+        for cand_m_L in Ml_space
+    )
+
+    if denominator == 0:
+        raise RuntimeError("joint_belief denominator is zero")
+
+    # commencer le calcul pour la distribution
     scores = {}
 
     for u in U_space:
-        scores[u] = get_speaker_score(m_S, tau_S, tau_L, u, Y_space, y_opt, k, n, w)
+        scores[u] = get_speaker_score(m_S, tau_S, tau_L, u, Y_space, y_opt, k, n, w, denominator, Ml_space)
 
     #softmax: reduce size to prevent overflow (e.g. something like exp(1000))
     max_score = max(scores.values())
@@ -46,20 +57,11 @@ def belief(w):
     #TODO: need to code in the real product terms here
     return
 
-def joint_belief(m_S, m_L, tau_S, tau_L, y, y_opt, m_L_space, w):
+def joint_belief(m_S, m_L, tau_S, tau_L, y, y_opt, joint_belief_den, w):
     numerator = (
         belief(w)
         * compatible(m_S, m_L, tau_S, tau_L, y_opt)
         * y_prior_given_mS_mL(m_S, m_L, tau_S, tau_L, y, y_opt)
     )
 
-    denominator = sum(
-        belief(w)
-        * compatible(m_S, cand_m_L, tau_S, tau_L, y_opt)
-        for cand_m_L in m_L_space
-    )
-
-    if denominator == 0:
-        raise RuntimeError("joint_belief denominator is zero")
-
-    return numerator / denominator
+    return numerator / joint_belief_den
